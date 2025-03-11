6742c6b118c9b2cc167247c4f53989260fb296ed
ew-er <thehatedbrosu@gmail.com>
2025-03-10 21:10:34 -0300

    Colocando pecas no tabuleiro

--- Csharp_chess/Program.cs
+++ Csharp_chess/Program.cs
@@ -1,5 +1,5 @@
 ﻿using tabuleiro;
-
+using xadrez;
 namespace Csharp_chess;
 
 class Program
@@ -7,6 +7,10 @@ class Program
     static void Main(string[] args)
     {
         Tabuleiro tab = new Tabuleiro(8, 8);
+        
+        tab.colocarPeca(new Torre(tab, Cor.preta), new Posicao(0,0));
+        tab.colocarPeca(new Torre(tab, Cor.preta), new Posicao(1,3));
+        tab.colocarPeca(new Rei(tab, Cor.preta), new Posicao(2,4));
         
         Tela.imprimirTabuleiro(tab);
     }

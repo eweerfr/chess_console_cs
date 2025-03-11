b2fa0be04e40193452861d1627b0d315cd531c46
ew-er <thehatedbrosu@gmail.com>
2025-03-10 23:36:37 -0300

    Criando a classe posicaoXadrez

---
+++ Csharp_chess/xadrez/PosicaoXadrez.cs
@@ -0,0 +1,25 @@
+using tabuleiro;
+
+namespace xadrez;
+
+public class PosicaoXadrez
+{
+    public char coluna { get; set; }
+    public int linha { get; set; }
+
+    public PosicaoXadrez(char coluna, int linha)
+    {
+        this.linha = linha;
+        this.coluna = coluna;
+    }
+
+    public Posicao toPosicao()
+    {
+        return new Posicao(8 - linha, coluna - 'a');
+    }
+    
+    public override string ToString()
+    {
+        return $"{coluna}{linha}";
+    }
+}
